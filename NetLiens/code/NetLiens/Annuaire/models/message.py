caea02f819c61c38ced859b480b739e7b4326f6a
Alex <hellboy94520@gmail.com>
2019-05-08 17:00:03 +0200

    [Models] Update all models in a new format
    [Models] Use Python class properties
    
    [Models] Update models to be all similar and avoir errors

--- NetLiens/code/NetLiens/Annuaire/models/message.py
+++ NetLiens/code/NetLiens/Annuaire/models/message.py
@@ -7,53 +7,66 @@ from django.contrib.auth.models import User
 Data
 ------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------- """
-class Message(models.Model):
-  sender        = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-  recipient     = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-  subject       = models.TextField()
-  date          = models.DateTimeField(default=datetime.now())
-  content       = models.TextField()
-
-  def __repr__(self):
-      return "Message : sender={}, recipient={}, subject={}, content={}".format(self.sender, self.recipient,
-                                                                                self.subject, self.content)
-
-  class Meta:
-    abstract = True
-
-
-class MessageUnread(Message):
-  pass
-
-class MessageRead(Message):
-  pass
+SubjectSize = 20
 
 
 """ --------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------
-Functions
+Class
 ------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------- """
-def create(pSender: User, pRecipient: User, pSubject, pContent: str):
+class Conversation(models.Model):
+  sender        = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+  recipient     = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+  subject       = models.CharField(max_length=SubjectSize)
 
-  if pSender.username == pRecipient.username:
-    return "sender: has the same username of recipient"
+  " --- Constructor --- "
+  def __init__(self, pSender: User, pRecipient: User, pSubject: str, pContent):
+    models.Model.__init__(self)
+    # Avoid to call my __setattr__ each time
+    object.__setattr__(self, 'sender'   , pSender)
+    object.__setattr__(self, 'recipient', pRecipient)
+    object.__setattr__(self, 'subject'  , pSubject)
+    self.save()
+    Message(True, pContent, self)
 
-  lMessage = MessageUnread()
-  lMessage.sender     = pSender
-  lMessage.recipient  = pRecipient
-  lMessage.subject    = pSubject
-  lMessage.date       = datetime.now()
-  lMessage.content    = pContent
-  lMessage.save()
+  " --- Setter --- "
+  def __setattr__(self, key, value):
+    object.__setattr__(self, key, value)
+    self.save()
 
-  return lMessage
+  " --- Shower --- "
+  def __repr__(self):
+    return "Conversation : sender={}, recipient={}, subject={}"\
+      .format(self.sender, self.recipient, self.subject)
+
+  " --- Return messages organized by date (most recent to old) --- "
+  def get_messages(self):
+    return Message.objects.filter(conv=self).order_by('-date')
 
 
-def message_read(pMessage: MessageUnread):
+""" ---------------------------------------------------------------------------------------------------------------- """
+class Message(models.Model):
+  is_sender = models.BooleanField()               # Indicate who send the message
+  is_read   = models.BooleanField(default=False)  # Indicate if the message is read
+  content   = models.TextField(max_length=SubjectSize)
+  conv      = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+  date      = models.DateTimeField(default=datetime.now())
 
-  lMessage = MessageRead.copy(pMessage)
-  lMessage.save()
-  pMessage.delete()
+  def __init__(self, pIsSender: bool, pContent: str, pConv: Conversation):
+    models.Model.__init__(self)
+    # Avoid to call my __setattr__ each time
+    object.__setattr__(self, 'is_sender'   , pIsSender)
+    object.__setattr__(self, 'is_read'     , False)
+    object.__setattr__(self, 'content'     , pContent)
+    object.__setattr__(self, 'conv'        , pConv)
+    object.__setattr__(self, 'date'        , datetime.now())
+    self.save()
 
-  return lMessage
+  def __setattr__(self, key, value):
+    object.__setattr__(self, key, value)
+    self.save()
+
+  def __repr__(self):
+    return "Message : is_sender={}, is_read={}, date={}, content={}"\
+      .format(str(self.is_sender), str(self.is_read), str(self.date), self.conv)
