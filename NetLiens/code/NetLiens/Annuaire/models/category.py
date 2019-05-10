e9ed75f5e801e2a3997dcc49b574b4ca62f70376
Alex <hellboy94520@gmail.com>
2019-05-10 21:58:32 +0200

    [Models] Update all models in a new format

--- NetLiens/code/NetLiens/Annuaire/models/category.py
+++ NetLiens/code/NetLiens/Annuaire/models/category.py
@@ -1,8 +1,7 @@
 from django.db import models
-from datetime import datetime
+from datetime import datetime, timedelta
 from .account import AccountAdmin
-from .communs import Log, Stat, CreationText, ModificationText
-from enum import Enum
+from .communs import Status, LogAdmin, Stat, CreationText, ModificationText, DeletionText
 
 from django.dispatch import receiver
 
@@ -14,17 +13,11 @@ Data
 CategoryTitleSize    = 100
 
 
-""" ---------------------------------------------------------------------------------------------------------------- """
-class Status(Enum):
-  UN = "Unknown"
-  AC = "Active"    # Show on the website
-  UA = "Unactive"  # Unshow on the website and preserve the data
-  DE = "Delete"   # Put the data in the trash
-
-
-""" ---------------------------------------------------------------------------------------------------------------- """
-
-
+""" --------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+Class
+------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------- """
 class Category(models.Model):
   nameFr    = models.CharField(max_length=CategoryTitleSize, default="")
   nameEn    = models.CharField(max_length=CategoryTitleSize, default="")
@@ -34,18 +27,94 @@ class Category(models.Model):
                                choices=[(tag, tag.value) for tag in Status],
                                default=Status.UN)
   children  = models.ManyToManyField('self', related_name='childrenCategory')
+  date      = models.DateField(default=datetime.now())    # Creation Date
+
+  " --- Constructor --- "
+  def __init__(self, pNameFr: str, pNameEn: str, pResumeFr: str, pResumeEn: str, pStatus: Status, pAdmin: AccountAdmin):
+    models.Model.__init__(self)
+    # Avoid to call deactivated __setattr__
+    object.__setattr__(self, 'nameFr'   , pNameFr   )
+    object.__setattr__(self, 'nameEn'   , pNameEn   )
+    object.__setattr__(self, 'resumeFr' , pResumeFr )
+    object.__setattr__(self, 'resumeEn' , pResumeEn )
+    object.__setattr__(self, 'status'   , pStatus   )
+    self.save()
+
+    # Creation CategoryStat and CategoryLog associate
+    CategoryStat(self)
+    CategoryLog("{} {}".format(CreationText, repr(self)),
+                self,
+                pAdmin)
+
+  def modif(self, pNameFr: str, pNameEn: str, pResumeFr: str, pResumeEn: str, pStatus: Status, pAdmin: AccountAdmin):
+    lLog = ModificationText
+    # Modification only if it is
+    if pNameFr and pNameFr != self.nameFr:
+      lLog += " nameFr: {},".format(pNameFr)
+      self.nameFr = pNameFr
+    if pNameEn and pNameEn != self.nameEn:
+      lLog += " nameEn: {},".format(pNameEn)
+      self.nameEn = pNameEn
+    if pResumeFr and pResumeFr != self.resumeFr:
+      lLog += " resumeFr: {},".format(pResumeFr)
+      self.resumeFr = pResumeFr
+    if pResumeEn and pResumeEn != self.resumeEn:
+      lLog += " resumeEn: {},".format(pResumeEn)
+      self.resumeEn = pResumeEn
+    if pStatus != Status.UN and pStatus != self.status:
+      lLog += " status : {},".format(pStatus)
+      self.status = pStatus
+    # Save the data
+    if lLog == ModificationText: return False   # If any modification has been done
+    self.save()
+    CategoryLog(lLog, self, pAdmin)
+
+  def to_trash(self, pAdmin: AccountAdmin):
+    # TODO: Faire le système qui va vérifier s'il faut supprimer un objet ou non dans la poubelle
+    self.status = Status.TR
+    self.date   = datetime.now()+timedelta(days=7)        # Date of deletion
+    CategoryLog("{} {}".format(DeletionText, repr(self)), pAdmin)
+    # TODO: Faire le système qui va déplacer les sites qui avait cette catégory par celle parent
+
+  def get_logs(self):
+    return CategoryLog.objects.filter(category=self)
+
+  def get_stat(self):
+    return CategoryStat.objects.get(category=self)
 
   def __repr__(self):
-    return "Category : nameFr={}, nameEn={}, resumeFr={}, resumeEn={}, status={}"\
-      .format(self.nameFr, self.nameEn, self.resumeFr, self.resumeEn, self.status.value)
+    return "Category : nameFr={}, nameEn={}, resumeFr={}, resumeEn={}, status={}, date={}"\
+      .format(self.nameFr, self.nameEn, self.resumeFr, self.resumeEn, self.status.value, self.date)
+
+  # Deactivate the modification by this way to be sure to have a log
+  def __setattr__(self, key, value):
+    pass
+
+  # Deactivate the deletion to have personnal system
+  def __del__(self):
+    pass
 
 
+""" ---------------------------------------------------------------------------------------------------------------- """
 class CategoryStat(Stat):
   category    = models.OneToOneField(Category, on_delete=models.CASCADE, primary_key=True)
 
+  def __init__(self, pCategory: Category):
+    Stat.__init__(self)
+    self.category = pCategory
+    self.save()
 
-class CategoryLog(Log):
-  category     = models.ForeignKey(Category     , on_delete=models.CASCADE)
+
+""" ---------------------------------------------------------------------------------------------------------------- """
+class CategoryLog(LogAdmin):
+  category     = models.ForeignKey(Category  , on_delete=models.CASCADE)
+
+  def __init__(self, pModif, pCategory: Category, pAdmin: AccountAdmin):
+    LogAdmin.__init__(self)
+    self.user     = pAdmin
+    self.modif    = pModif
+    self.category = pCategory
+    self.save()
 
 
 """ --------------------------------------------------------------------------------------------------------------------
@@ -53,101 +122,13 @@ class CategoryLog(Log):
 Functions
 ------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------- """
-" --- Creation of stat --- "
-def create_stat(pCategory: Category):
-  lCategory = CategoryStat()
-  lCategory.category = pCategory
-  lCategory.save()
-  return lCategory
-
-
-" --- Creation of log --- "
-def create_log(pUser: AccountAdmin, pModification: str, pCategory: Category):
-  lLog = CategoryLog()
-  lLog.date       = datetime.now()
-  lLog.modif      = pModification
-  lLog.user       = pUser
-  lLog.category   = pCategory
-  lLog.save()
-  return lLog
-
-
-" --- Creation of category --- "
-def create(pNameFr: str, pNameEn: str, pResumeFr: str, pResumeEn: str, pStatus: Status, pUser: AccountAdmin):
-
-  lCategory = Category()
-  lCategory.nameFr   = pNameFr
-  lCategory.nameEn   = pNameEn
-  lCategory.resumeFr = pResumeFr
-  lCategory.resumeEn = pResumeEn
-  lCategory.status   = pStatus
-  lCategory.save()
-
-  create_stat(lCategory)
-  create_log(pUser, "{} {}".format(CreationText, repr(lCategory)), lCategory)
-
-  return lCategory
-
-
-" --- Modification of category --- "
-def modif(pCategory: Category, pNameFr: str, pNameEn: str, pResumeFr: str, pResumeEn: str, pStatus: Status,
-          pUser: AccountAdmin):
-
-  lLog = ModificationText
-  lModif = False
-
-  if pNameFr and pNameFr != pCategory.nameFr:
-    lLog += " nameFr: {},".format(pNameFr)
-    pCategory.nameFr  = pNameFr
-    lModif = True
-
-  if pNameEn and pNameEn != pCategory.nameEn:
-    lLog += " nameEn: {},".format(pNameEn)
-    pCategory.nameEn  = pNameEn
-    lModif = True
-
-  if pResumeFr and pResumeFr != pCategory.resumeFr:
-    lLog += " resumeFr: {},".format(pResumeFr)
-    pCategory.resumeFr  = pResumeFr
-    lModif = True
-
-  if pResumeEn and pResumeEn != pCategory.resumeEn:
-    lLog += " resumeEn: {},".format(pResumeEn)
-    pCategory.resumeEn = pResumeEn
-    lModif = True
-
-  if pStatus != Status.UN and pStatus != pCategory.status:
-    lLog += " status : {},".format(pStatus)
-    pCategory.status = pStatus
-    lModif = True
-
-  # If any modification has been done
-  if not lModif: return False
-  # Else
-  pCategory.save()
-  create_log(pUser, lLog, pCategory)
-  return pCategory
-
-
+# TODO: Me renseigner sur le fait de voir si c'est possible de mettre cette fonction dans le classe sachant que je peux pas avoir l'enfant en Category
 " --- Add Children category to Parent category --- "
 def add_children(pParent: Category, pChildren: Category, pUser: AccountAdmin):
   pParent.children.add(pChildren)
   pParent.save()
-  create_log(pUser, "Add Children {}".format(repr(pChildren)))
+  CategoryLog("Add Children {}".format(repr(pChildren)), pParent, pUser)
   return True
-
-
-" --- Get logs associate to category --- "
-def get_logs(pCategory: Category):
-  return CategoryLog.objects.filter(category=pCategory)
-
-
-" --- Get stat associate to category --- "
-def get_stat(pCategory: Category):
-  lResult = CategoryStat.objects.filter(category=pCategory)
-  if len(lResult) != 1:
-    return "Error : Any or too many results !"
-  return lResult[0]
 
 
 " --- Get the much see category --- "
