ddff82c2509652e0208904c1904336a3075a3839
Alex <hellboy94520@gmail.com>
2019-07-20 15:58:31 +0200

    Correction of models

--- NetLiens/code/NetLiens/Annuaire/models/__init__.py
+++ NetLiens/code/NetLiens/Annuaire/models/__init__.py
@@ -2,7 +2,7 @@ from .account import *
 from .category import *
 from .localisation import *
 #from .keyword import *
-#from .site import *
+from .site import *
 from .stat import *
-#from .payment import *
+from .payment import *
 #from .message import *
